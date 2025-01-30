287f615bee359cd16d2b31c0003f7afdbc3b0ed6
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2025-01-30 22:29:06 +0000

    remove unused EventHandlers class

--- dotnet/src/App/EventHandlers.cs
+++
@@ -1,9 +0,0 @@
-
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-
-[EventHandler("onenterpressed", typeof(KeyboardEventArgs), 
-    enableStopPropagation: true, enablePreventDefault: true)]
-public static class EventHandlers
-{
-}
